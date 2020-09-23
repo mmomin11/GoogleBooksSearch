6c754d948172753ea48796b6ecd3123716af9a49
mmomin11 <mubin.momin@nxp.com>
2020-09-23 14:56:06 -0500

    final route issues solved

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Form from '../components/Form';
-import Results from '../components/Results';
+import Form from '../components/Form/Form';
+import Results from '../components/Results/index';
 import API from '../utils/API';
 
 class Search extends React.Component {
@@ -63,3 +63,5 @@ class Search extends React.Component {
 		);
 	}
 }
+
+export default Search;
